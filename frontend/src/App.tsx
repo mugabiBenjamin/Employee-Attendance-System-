f8c03e50bd433186a1ed36d377fba71872ef23dc
mugabiBenjamin <mugabiben6@gmail.com>
2025-07-30 14:08:00 +0300

    Initial setup for frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import { Button } from "./components/ui/button";
+
+export default function App() {
+  return (
+    <div className="flex flex-col items-center my-3">
+      <Button size={"lg"}>Click Me</Button>
+    </div>
+  );
+}
